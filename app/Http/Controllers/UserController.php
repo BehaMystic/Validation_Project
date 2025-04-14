75b8996aa49b5e1590aa4957ebc50664c53ec72b
Behruz.Imamov <lakator08@gmail.com>
2025-04-14 20:53:59 +0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,15 +30,15 @@ class UserController extends Controller
     public function store(StoreUserRequest $request)
     {
       $request->validate([
-             'name' => 'required|string|min:6',
-             'email' => 'required|email',
-        'age' => 'required|integer|min:20',
-             'date' => 'required|date',
-             'password' => 'required|min:8|confirmed',
-             'url' => 'nullable|url',
-             'image' => 'nullable|file|mimes:jpeg,png,jpg|max:2048',
-             'product_name' => 'required|string|min:5',
-             'price' => 'required|numeric|min:0.01',
+             //'name' => 'required|string|min:6',
+             //'email' => 'required|email',
+     //   'age' => 'required|integer|min:20',
+           //  'date' => 'required|date',
+           ///  'password' => 'required|min:8|confirmed',
+           //  'url' => 'nullable|url',
+            // 'image' => 'nullable|file|mimes:jpeg,png,jpg|max:2048',
+           //  'product_name' => 'required|string|min:5',
+           //  'price' => 'required|numeric|min:0.01',
      ]);
     
     return $request->all();
